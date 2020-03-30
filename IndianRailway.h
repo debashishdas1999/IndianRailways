1f022a4d69dd45ce8661b3c9ed44eb8de3bc7b2a
debashishdas1999 <47416666+debashishdas1999@users.noreply.github.com>
2020-03-30 22:56:36 +0530

    Complete implementation of module

--- IndianRailway.h
+++ IndianRailway.h
@@ -11,4 +11,8 @@ struct station {
 
 void station_init(struct station *station);
 
+void station_load_train(struct station *station, int count);
 
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
