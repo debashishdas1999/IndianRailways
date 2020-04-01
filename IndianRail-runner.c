feb2476ae27907ef0136bde9a1933f3c594940f5
debashishdas1999 <47416666+debashishdas1999@users.noreply.github.com>
2020-04-01 19:23:50 +0530

    Update and rename IndianRailway-runner.c to IndianRail-runner.c

--- IndianRail-runner.c
+++ IndianRail-runner.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#include "IndianRailway.h"
+#include "IndianRail.h"
 
 // Count of passenger threads that have completed (i.e. station_wait_for_train
 // has returned) and are awaiting a station_on_board() invocation.
